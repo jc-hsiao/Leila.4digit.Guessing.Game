f306c2b097b619ea8247638650bd6486208aefc4
Leila Hsiao <lsiao@One-of-the-Zipcodes-MacBookssssss.local>
2020-03-24 10:32:08 -0400

    fix readme

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,22 @@
 public class Game {
+
+    String answer;
+
+    public Game(){
+        answer = generateNumberSet();
+    }
+
+    public String generateNumberSet(){
+        return "";
+    }
+
+    public int countAs( String guessed ){
+        return 0;
+    }
+
+    public int countBs( String guessed ){
+        return 0;
+    }
+
+
 }
